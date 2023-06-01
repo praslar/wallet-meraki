892807eed3405b12c1aa4635cb23dabddd17ef14
praslar <pxtha@merakilab.space>
2023-06-01 13:57:20 +0700

    feat(meraki-22): Define role table, add role column to user table

--- internal/model/user.go
+++ internal/model/user.go
@@ -7,7 +7,14 @@ type User struct {
 	ID       uuid.UUID `json:"id" gorm:"primaryKey;default:uuid_generate_v4()"`
 	Email    string    `json:"email"`
 	Password string    `json:"password"`
+	Roles    uint8     `json:"roles"  gorm:""`
 	Wallets  []Wallet  `json:"wallets"`
+}
+
+type Role struct {
+	BaseModel
+	Name  string `json:"name"`
+	Value uint8  `json:"value"`
 }
 
 type UserRequest struct {
