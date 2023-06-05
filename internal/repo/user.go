e3d9fdbf73516f2a7c376f27153c095dcf859971
praslar <pxthang97@gmail.com>
2023-06-05 15:44:42 +0700

    feat(meraki-43):add repo func getUserbyemail

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -18,3 +18,11 @@ func NewUserRepo(db *gorm.DB) UserRepo {
 func (r *UserRepo) CreateUser(user *model.User) error {
 	return r.db.Create(user).Error
 }
+
+func (r *UserRepo) GetUserByEmail(email string) (*model.User, error) {
+	var user model.User
+	if err := r.db.Model(&model.User{}).Where("email = ?", email).Take(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
