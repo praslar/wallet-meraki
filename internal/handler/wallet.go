a25270554111015ce5346f6b8948fcd57792fdce
Good <giathinh1112@gmail.com>
2023-06-16 00:06:02 +0700

    add api createwallet

--- internal/handler/wallet.go
+++ internal/handler/wallet.go
@@ -1,1 +1,77 @@
 package handler
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"strings"
+	"wallet/internal/model"
+	"wallet/internal/service"
+)
+
+type WalletHandler struct {
+	WalletService service.WalletService
+	authService   service.AuthService
+}
+
+func NewWalletHandler(WalletService service.WalletService, authService service.AuthService) WalletHandler {
+	return WalletHandler{
+		WalletService: WalletService,
+		authService:   authService,
+	}
+}
+
+func (h *WalletHandler) CreateWallet(w http.ResponseWriter, r *http.Request) {
+	requestWallet := model.WalletRequest{}
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewDecoder(r.Body).Decode(&requestWallet)
+	if err != nil {
+		logrus.Errorf("Failed to get request body: %v", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	jwtToken := r.Header.Get("Authorization")
+	token := strings.Split(jwtToken, " ")
+	if token[0] != "Bearer" {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "unauthorized jwt",
+		})
+		return
+	}
+
+	// jwtToken
+	if err := h.authService.ValidJWTToken(token[1], "user"); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "unauthorized role",
+		})
+		return
+	}
+
+	if err := h.authService.ValidJWTTokenXuser(token[1], requestWallet.UserID); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "unauthorized role",
+		})
+		return
+	}
+
+	if err := h.WalletService.CreateWallet(requestWallet.Name, requestWallet.UserID); err != nil {
+		logrus.Errorf("Failed create wallet: %v", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err = json.NewEncoder(w).Encode(requestWallet); err != nil {
+		return
+	}
+}
--- internal/service/wallet.go
+++ internal/service/wallet.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"wallet/internal/model"
+	"wallet/internal/repo"
+)
+
+type WalletService struct {
+	WalletRepo  repo.WalletRepo
+	authService AuthService
+}
+
+func NewWalletService(WalletRepo repo.WalletRepo, authService AuthService) WalletService {
+	return WalletService{
+		WalletRepo:  WalletRepo,
+		authService: authService,
+	}
+}
+
+func (s *WalletService) CreateWallet(name string, userID uuid.UUID) error {
+
+	if s.WalletRepo.CheckWalletExist(name) {
+		return fmt.Errorf("Wallet existed")
+	}
+
+	newWallet := &model.Wallet{
+		Name:   name,
+		UserID: userID,
+	}
+
+	if err := s.WalletRepo.CreateWallet(newWallet); err != nil {
+		logrus.Errorf("Failed to create new wallet: %s", err.Error())
+		return fmt.Errorf("Internal server error")
+	}
+	return nil
+
+}
