52a449b50b42f6aa883967efcd14eae5e972f919
Nguyễn Xuân Vũ <60343954+ngxvu@users.noreply.github.com>
2023-06-15 22:18:23 +0700

    meraki-feat32-admincreatetoken

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"fmt"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 	"wallet/internal/model"
 )
@@ -16,11 +18,11 @@ func NewUserRepo(db *gorm.DB) UserRepo {
 }
 
 func (r *UserRepo) CreateUser(user *model.User) error {
-	return r.db.Create(user).Error
+	return r.db.Create(&user).Error
 }
 
 func (r *UserRepo) GetAllUser() ([]model.User, error) {
-	rs := []model.User{}
+	var rs []model.User
 	if err := r.db.Find(&rs).Error; err != nil {
 		return nil, err
 	}
@@ -29,16 +31,37 @@ func (r *UserRepo) GetAllUser() ([]model.User, error) {
 
 func (r *UserRepo) GetUserByEmail(email string) (*model.User, error) {
 	var user model.User
-	if err := r.db.Model(&model.User{}).Where("email = ?", email).Take(&user).Error; err != nil {
+	if err := r.db.Model(&model.User{}).Where("email = ?", email).Preload("Role").First(&user).Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
 }
 
 func (r *UserRepo) GetUserByID(id string) (*model.User, error) {
-	var user model.User
+	var user *model.User
+	fmt.Print(r.db.Name())
 	if err := r.db.Model(&model.User{}).Where("id = ?", id).Take(&user).Error; err != nil {
 		return nil, err
 	}
-	return &user, nil
+	return user, nil
+}
+
+func (r *UserRepo) CreateToken(newToken *model.Token) error {
+	result := r.db.Create(&newToken)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *UserRepo) SymbolUnique(symbol string) bool {
+	var token *model.Token
+	result := r.db.Model(&model.Token{}).Where("symbol = ?", symbol).Find(&token)
+	if result != nil {
+		logrus.Infof("Khong tim thấy token. ")
+		return false
+		// tim ko co thi chua co token
+	}
+	return true
+	// tim co thi co token
 }
