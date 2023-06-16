f84ef2296afac19849d32b9cbcd495b27ff03c37
Good <giathinh1112@gmail.com>
2023-06-16 19:40:17 +0700

    add func createwallet

--- internal/handler/wallet.go
+++ internal/handler/wallet.go
@@ -1,6 +1,12 @@
 package handler
 
-import "wallet/internal/service"
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"wallet/internal/model"
+	"wallet/internal/service"
+)
 
 type WalletHandler struct {
 	WalletService service.WalletService
@@ -10,4 +16,31 @@ func NewWalletHandler(WalletService service.WalletService) WalletHandler {
 	return WalletHandler{
 		WalletService: WalletService,
 	}
+}
+
+func (h *WalletHandler) CreateWallet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	requestWallet := model.WalletRequest{}
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewDecoder(r.Body).Decode(&requestWallet)
+	if err != nil {
+		logrus.Errorf("Failed to get request body: %v", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := h.WalletService.CreateWallet(requestWallet.UserID, requestWallet.Name); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logrus.Errorf(err.Error())
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "Internal server error",
+		})
+		return
+	}
+
 }
--- internal/service/wallet.go
+++ internal/service/wallet.go
@@ -1,6 +1,12 @@
 package service
 
-import "wallet/internal/repo"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"wallet/internal/model"
+	"wallet/internal/repo"
+)
 
 type WalletService struct {
 	WalletRepo repo.WalletRepo
@@ -10,4 +16,23 @@ func NewWalletService(WalletRepo repo.WalletRepo) WalletService {
 	return WalletService{
 		WalletRepo: WalletRepo,
 	}
+}
+
+func (s *WalletService) CreateWallet(userID uuid.UUID, name string) error {
+
+	if s.WalletRepo.CheckWalletExist(name) {
+		return fmt.Errorf("Wallet existed")
+	}
+
+	newWallet := &model.Wallet{
+		Name:   name,
+		UserID: userID,
+	}
+
+	if err := s.WalletRepo.CreateWallet(newWallet); err != nil {
+		logrus.Errorf("Failed to create new wallet: %s", err.Error())
+		return fmt.Errorf("Internal server error")
+	}
+	return nil
+
 }
