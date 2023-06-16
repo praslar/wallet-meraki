767c0b40f174af487b541585a68bca35c4f07ee4
praslar <pxthang97@gmail.com>
2023-06-16 12:16:29 +0700

    add author middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -19,6 +20,19 @@ func AuthenMiddleware(next http.Handler) http.Handler {
 			})
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func AuthourMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		jwtToken := r.Header.Get("x-user-id")
+		// TODO:
+		// get user current roles
+		// check if user has role admin
+		// if not, return unauthorized
+		fmt.Println(jwtToken)
 		next.ServeHTTP(w, r)
 	})
 }
