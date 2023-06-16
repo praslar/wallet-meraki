d713fe9f8bb35f181392d22bff8a9a2dc8a180af
Good <giathinh1112@gmail.com>
2023-06-16 20:54:52 +0700

    meraki-27/add func getallwallet

--- internal/handler/wallet.go
+++ internal/handler/wallet.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"strconv"
 	"wallet/internal/model"
 	"wallet/internal/service"
 )
@@ -72,4 +73,29 @@ func (h *WalletHandler) GetOneWallet(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(rs)
 
+}
+
+func (h *WalletHandler) GetAllWallet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	order := r.URL.Query().Get("order")
+	name := r.URL.Query().Get("name")
+	userID := r.URL.Query().Get("user_id")
+
+	pageSize := r.URL.Query().Get("page_size")
+	pageSizeInt, _ := strconv.Atoi(pageSize)
+
+	page := r.URL.Query().Get("page")
+	pageInt, _ := strconv.Atoi(page)
+
+	wallets, err := h.WalletService.GetAllWallet(order, name, userID, pageSizeInt, pageInt)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(wallets)
 }
