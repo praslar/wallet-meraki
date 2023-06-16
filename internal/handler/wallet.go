381eb88deefaf7b9cad9e8c13ceeed744f9bb3b7
Good <giathinh1112@gmail.com>
2023-06-16 19:39:17 +0700

    oop wallethandler

--- internal/handler/wallet.go
+++ internal/handler/wallet.go
@@ -1,1 +1,13 @@
 package handler
+
+import "wallet/internal/service"
+
+type WalletHandler struct {
+	WalletService service.WalletService
+}
+
+func NewWalletHandler(WalletService service.WalletService) WalletHandler {
+	return WalletHandler{
+		WalletService: WalletService,
+	}
+}
--- internal/service/wallet.go
+++ internal/service/wallet.go
@@ -1,1 +1,13 @@
 package service
+
+import "wallet/internal/repo"
+
+type WalletService struct {
+	WalletRepo repo.WalletRepo
+}
+
+func NewWalletService(WalletRepo repo.WalletRepo) WalletService {
+	return WalletService{
+		WalletRepo: WalletRepo,
+	}
+}
