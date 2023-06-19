eb33b94d9ee955e230a42f4fe1b2520c321f5698
Nguyễn Xuân Vũ <60343954+ngxvu@users.noreply.github.com>
2023-06-19 18:26:42 +0700

    fix run code

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -156,7 +156,6 @@ func (h *UserHandler) GetOne(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func (h *UserHandler) ViewTransaction(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	currentUser := r.Header.Get("x-user-id")
@@ -173,8 +172,11 @@ func (h *UserHandler) ViewTransaction(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(map[string]interface{}{
 		"data": users,
 	}); err != nil {
+		return
+	}
+	return
+}
 
-    
 func (h *UserHandler) CreateToken(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	requestToken := model.TokenRequest{}
@@ -249,7 +251,6 @@ func (h *UserHandler) DeleteToken(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func (h *UserHandler) GetListAllTransaction(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	//su dung ham take trong mux
@@ -275,10 +276,10 @@ func (h *UserHandler) GetListAllTransaction(w http.ResponseWriter, r *http.Reque
 	if err = json.NewEncoder(w).Encode(map[string]interface{}{
 		"data": result,
 	}); err != nil {
-     return
-  }
+		return
+	}
 }
-    
+
 func (h *UserHandler) UpdateToken(w http.ResponseWriter, r *http.Request) {
 	requestToken := model.TokenRequest{}
 	w.Header().Set("Content-Type", "application/json")
