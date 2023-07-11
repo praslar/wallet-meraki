d9e6b04026c268aa6b0df68833288b6e88c73fc7
praslar <pxtha@merakilab.space>
2023-07-11 13:54:48 +0700

    add hello world

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"wallet/internal/handler"
 	"wallet/internal/middleware"
@@ -18,6 +19,7 @@ func main() {
 
 	config.SetEnv()
 
+	fmt.Println("hello world")
 	db, err := pg.ConnectDB(config.AppConfig{
 		DBHost:     config.LoadEnv().DBHost,
 		DBPort:     config.LoadEnv().DBPort,
